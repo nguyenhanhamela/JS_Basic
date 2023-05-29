321af0dad9e7c032a03c70a2f6c084958583c820
trang.ta <trangta.0896@gmail.com>
2023-05-29 15:59:19 +0700

    Trang commit btvn operator

---
+++ Trang_lesson3/operators/1.js
@@ -0,0 +1,9 @@
+// Áp dụng toán tử ++, tăng biến counter lên 1 đơn vị ở dòng …. trong đoạn code sau: 
+// let counter = 10; 
+// ... 
+// console.log( counter ); // 11 
+
+let counter = 10;
+// counter++;
+++counter;
+console.log(counter);
--- Trang_lesson3/variables/10.js
+++ Trang_lesson3/variables/10.js
@@ -1,7 +1,7 @@
 // Các biến nào dưới đây được đặt tên đúng. 
 // Trả lời bằng cách  thêm câu trả lời vào comment sau mỗi dòng code. Ví dụ: let y  = 10 ; // tên biến đặt đúng 
 
-var 123 = 40; // sai vì trong tên biến không được bắt đầu bằng số
-var *aa = 250; // sai vì tên biến không được bắt đầu bằng *
+// var 123 = 40; // sai vì trong tên biến không được bắt đầu bằng số
+// var *aa = 250; // sai vì tên biến không được bắt đầu bằng *
 var _x_1 = 7; //đúng
 var _value = "test"; //đúng 
