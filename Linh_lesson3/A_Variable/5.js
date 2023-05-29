b489f3f657126a76946e5a0af4a4f70896c0a26d
Linh Le <lelinh209@gmail.com>
2023-05-29 16:13:40 +0700

    Bài tập 5

--- Linh_lesson3/A_Variable/5.js
+++ Linh_lesson3/A_Variable/5.js
@@ -1,8 +1,7 @@
-// var firstName = "my"
-// var lastName = "name"
-// var age = 30
+var firstName = "my", lastName = "name", age = 30
 
 //var Fullname = firstName + lastName + age
 //console.log(Fullname)
-firstName, lastName, age = "my", "name", 30
-//console.log(firstName,lastName,age)
+//firstName, lastName, age = "my", "name", 30
+
+console.log(firstName,lastName,age)
