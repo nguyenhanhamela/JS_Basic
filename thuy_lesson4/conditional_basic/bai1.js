46b0c2573890c0616246716fe80663ab26289708
ThuyNguyen <thuy83.tmu@gmail.com>
2023-06-05 23:41:26 +0700

    conditional basic: bai 1, 2

---
+++ thuy_lesson4/conditional_basic/bai1.js
@@ -0,0 +1,13 @@
+/**BAI 1: Cho 2 số: num_1 = 10, num_2 = 5.
+ * Sử dụng câu lệnh rẽ nhanh if/else để log ra màn hình số lớn nhất trong 2 số trên. */
+
+let num_1 = 10;
+let num_2 = 5;
+
+if (num_1 > num_2) {
+  console.log(`So lon nhat la: ${num_1}`);
+} else if (num_1 < num_2) {
+  console.log(`So lon nhat la: ${num_2}`);
+} else {
+  console.log(`Hai so bang nhau!`);
+}
---
+++ thuy_lesson4/conditional_basic/bai2.js
@@ -0,0 +1,31 @@
+const language = ["vn", "en", "de", "us"];
+
+// PRACTICE: FOR
+for (let i = 0; i < language.length; i++) {
+  console.log(language[i]); // output : vn en de us
+}
+
+for (let j in language) {
+  console.log(j); // output : 0 1 2 3
+}
+
+for (let k of language) {
+  console.log(k); // output : vn en de us
+}
+
+// BAI2: In ra màn hình “Hello World” với 4 ngôn ngữ trên, mặc định là Tiếng Anh
+for (let lg of language) {
+  switch (lg) {
+    case "vn":
+      console.log("Chào thế giới");
+      break;
+    case "de":
+      console.log("Hello world");
+      break;
+    case "us":
+      console.log("Hallo Welt");
+      break;
+    default:
+      console.log("Hello world");
+  }
+}
