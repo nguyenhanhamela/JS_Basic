e77f1d24e75a60c99b3a6e10d48c90a4336cedc8
Haiyen126 <haiyen20612@gmail.com>
2023-06-07 21:12:31 +0700

    BT2

--- NTHYen_lesson4/Conditional-Basic/bt2.js
+++ NTHYen_lesson4/Conditional-Basic/bt2.js
@@ -1,2 +1,22 @@
 // 2. Cho 1 mảng sau const language = ["vn", "en", "de", "us"]. 
 // In ra màn hình “Hello World” với 4 ngôn ngữ trên, mặc định là Tiếng Anh
+
+const language = ["vn", "en", "de", "us"];
+let hello = "Hello World";
+for (let i = 0; i < language.length; i++) {
+  switch(language[i]) {
+    case "vn":
+      hello = "Xin chào thế giới";
+      break;
+    case "de":
+      hello = "Hallo Welt";
+      break;
+    case "us":
+      hello = "Hello World";
+      break;
+    default:
+      hello = "Hello World";
+  }
+  console.log(hello);
+}
+
